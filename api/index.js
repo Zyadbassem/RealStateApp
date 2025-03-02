fbc7bf2f390a9f9949d62857c5cae73b561b764c
zyad <zyadbassem9090@gmail.com>
2025-03-02 20:41:16 +0200

    connect the mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Connect to MongoDB
+dotenv.config();
+const mongo_url = process.env.MONGO_URL;
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 const app = express();
-
 app.listen(3000, () => {
   return console.log("Server is running on http://localhost:3000");
 });
